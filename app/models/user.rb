a92d3858e5c4ba163dc52a9c70027441124963c3
Jose C Fernandez <josefernandez@joseworks.org>
2023-06-09 08:14:25 -0400

    Setup user model and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  require 'jwt'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  EMAIL_RGX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, format: { with: EMAIL_RGX }, uniqueness: { case_sensitive: false }, allow_blank: false
 end
