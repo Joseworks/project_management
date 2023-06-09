e846bc6bae6a9092dc1f0990f465a48e17b5ca65
Jose C Fernandez <josefernandez@joseworks.org>
2023-06-09 10:21:43 -0400

    Add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,5 +4,16 @@ require 'rails_helper'
 
 describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+
+  context 'with a valid user' do
+    let(:user) { create(:user) }
+    let(:token) { user.generate_jwt }
+    let(:key) { Rails.application.secrets.secret_key_base }
+
+    it 'generates a valid jwt token' do
+      expect { JWT.decode(token, key) }.not_to raise_error(JWT::DecodeError)
+    end
+  end
 end
