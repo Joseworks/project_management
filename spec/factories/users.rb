2edf9c0d3a80d58a87ccde4868077ba7a7b87588
Jose C Fernandez <josefernandez@joseworks.org>
2023-06-09 10:21:10 -0400

    Add Faker gem. Setup user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@
 
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "username#{n}@example.com" }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length: 10, max_length: 20, mix_case: false, special_characters: true) }
   end
 end
